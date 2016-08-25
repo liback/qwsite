674df9c516f538e5b306fbc29e2f4f6ba661da0d
Linus Bäckman <linus@refragged.com>
2016-08-25 11:48:26 +0200

    Prev and Next link now stays on same line in lower resolution/window.

--- resources/views/map/show.blade.php
+++ resources/views/map/show.blade.php
@@ -7,14 +7,14 @@
 <div class="panel-body">
 	<div class="col-sm-12" style="margin: 0 0 1em 0;">
 		<div class="row">
-			<div class="col-sm-6">
+			<div class="col-xs-6 text-left">
 				<?php if($prevMap):?>
 					<a href="{{ URL::to('map/'. $prevMap) }}" class="btn btn-lg btn-default"><< Previous</a>
 				<?php endif;?>
 			</div>
-			<div class="col-sm-6">
+			<div class="col-xs-6 text-right">
 				<?php if($nextMap): ?>
-					<a href="{{ URL::to('map/'. $nextMap) }}" class="btn btn-lg btn-default pull-right">Next >></a>
+					<a href="{{ URL::to('map/'. $nextMap) }}" class="btn btn-lg btn-default">Next >></a>
 				<?php endif;?>
 			</div>
 		</div>
